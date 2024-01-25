506e6ff443616697cd13ac9db36fd9c0d4680193
Kevin-M254 <kmburu007@gmail.com>
2024-01-25 15:51:31 +0300

    printing opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <udis86.h>
 
 /**
  * main - prints the opcodes of its own main function
@@ -11,29 +10,34 @@
  */
 int main(int argc, char *argv[])
 {
-	ud_t ud_obj;
-	int val = 0, i = 0;
+	int index, bytes;
+	int (*address)(int, char **) = main;
+	unsigned char opcode;
 
-	if (argc == 2)
+	if (argc !=2)
 	{
-		val = atoi(argv[1]);
-
-		if (val < 0)
-		{
-			printf("Error\n");
-			exit(2);
-		}
-
-		ud_unit(&ud_obj);
-		ud_set_input_buffer(&ud_obj, argv[1], val);
-		ud_set_mode(&ud_obj, 64);
-		ud_set_syntax(&ud_obj, UD_SYN_INTEL);
-
-		while (ud_disassemble(&ud_obj))
-		{
-			printf("\t%s\n", ud_isn_hex(&ud_obj));
-		}
+		printf("Error\n");
+		exit(1);
 	}
 
-	return (0);
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (index = 0; index < bytes; index++)
+	{
+		opcode = *(unsigned char *)address;
+		printf("%.2x", opcode);
+
+		if (index == bytes - 1)
+			continue;
+		printf(" ");
+		address++;
+	}
+	printf("\n");
+	return(0);
 }
